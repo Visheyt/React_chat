ccc366a732b5ca2f08f6e628ee5b5c021ada247d
Alex <fornikaras7@gmail.com>
2025-01-06 01:51:21 +0500

    feat: reverse messages

--- src/features/chat/components/messages-list/messages-list.tsx
+++ src/features/chat/components/messages-list/messages-list.tsx
@@ -14,7 +14,7 @@ export const MessagesList = () => {
     (state: RootState) => state.chatReducer.messages
   );
 
-  const messagesValues = Object.values(messages);
+  const messagesValues = Object.values(messages).reverse();
 
   return (
     <div className={styles.container}>
