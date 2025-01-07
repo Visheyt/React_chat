7f98be64cdbac8aa3dffad7e48cab00009e77f67
Alex <fornikaras7@gmail.com>
2025-01-08 00:46:29 +0500

    feat: add about us page

--- src/features/about/about.tsx
+++ src/features/about/about.tsx
@@ -1,5 +1,18 @@
+import { Button } from "antd";
 import styles from "./about.module.scss";
+import { useNavigate } from "react-router";
 
 export const AboutPage = () => {
-  return <div className={styles.container}>ABOUT PAGE</div>;
+  const navigate = useNavigate();
+  return (
+    <div className={styles.container}>
+      <p>
+        This is my test tutorial chat app on React, there is a similar app on
+        angular. You can exchange messages, edit and delete them, thanks to Rs
+        School for the server and its implementation. Good luck in communicating
+        with friends and thanks for using my app.
+      </p>
+      <Button onClick={() => navigate("/login")}>Go to login page</Button>
+    </div>
+  );
 };
